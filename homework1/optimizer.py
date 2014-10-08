bfb586c2ca7e830d7c21c8a678d41db0657ed84f
Dave Anderson <dave@zemagood.com>
2014-10-07 22:42:46 -0400

    portfolio optimizer

---
+++ homework1/optimizer.py
@@ -0,0 +1,57 @@
+# QSTK Imports
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+
+# Third Party Imports
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main():
+
+    # List of symbols
+    ls_symbols = ["GOOG", "AAPL", "GLD", "XOM"]
+
+    # List of allocation percentages
+    ls_alloc = [0.2, 0.3, 0.4, 0.1]
+
+    dt_start = dt.datetime(2011, 1, 1)
+    dt_end = dt.datetime(2011, 1, 10)
+
+    vol, daily_ret, sharpe, cum_ret = simulate(dt_start, dt_end, ls_symbols, ls_alloc)
+
+
+def simulate(dt_start, dt_end, ls_symbols, ls_alloc):
+
+    # We need closing prices so the timestamp should be hours=16.
+    dt_timeofday = dt.timedelta(hours=16)
+
+    # Get a list of trading days between the start and the end.
+    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+
+    # Creating an object of the dataaccess class with Yahoo as the source.
+    c_dataobj = da.DataAccess('Yahoo')
+
+    # Keys to be read from the data, it is good to read everything in one go.
+    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+
+    # Reading the data, now d_data is a dictionary with the keys above.
+    # Timestamps and symbols are the ones that were specified before.
+    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+    d_data = dict(zip(ls_keys, ldf_data))
+
+    # Getting the numpy ndarray of close prices.
+    na_price = d_data['close'].values
+
+    # Normalizing the prices to start at 1 and see relative returns
+    na_normalized_price = na_price / na_price[0, :]
+
+    # Copy the normalized prices to a new ndarry to find returns.
+    na_rets = na_normalized_price.copy()
+
+    # Calculate the daily returns of the prices. (Inplace calculation)
+    # returnize0 works on ndarray and not dataframes.
+    tsu.returnize0(na_rets)
+
+    return vol, daily_ret, sharpe, cum_ret
